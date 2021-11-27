af315da823663a88f4672ce81ddec62ef2f3bbb9
DevSakazaki <84921425+DevSakazaki@users.noreply.github.com>
2021-11-27 09:52:23 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:precobitcoin/home.dart';
+
+void main() => runApp(
+  const MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  )
+);
